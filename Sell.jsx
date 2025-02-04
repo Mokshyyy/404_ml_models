f5154b7eaaf148d3e77cf7ac08c01444845228e0
adityapathak9450 <adityapathak9450@gmail.com>
2025-02-04 18:13:22 +0530

    Add files via upload

---
+++ Sell.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const Sell = ()=>{
+
+return  <>
+        <div>
+
+          things to be updated here 
+
+
+        </div>
+
+
+</>
+
+
+}
+
+export default Sell;
