7dbc29381a18e7e1ca171a1ffdddcc40a9b76f37
Mokshyyy <mokshgiri9@gmail.com>
2025-02-05 22:51:55 +0530

    DelModel.py

--- DelModel.py
+++ DelModel.py
@@ -8,6 +8,7 @@ import numpy as np
 import re
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import pickle
 # making dataframes
 raw_df = pd.read_csv('Delhi_v2.csv')
 df = raw_df[['price','area','Bedrooms','Bathrooms','neworold']].dropna()
@@ -20,3 +21,8 @@ model = LinearRegression()
 inputs = df[['area','Bedrooms','Bathrooms']]
 targets = df['price']
 model.fit(inputs,targets)
+prediction = model.predict([[1200,2,2]])
+print(prediction)
+
+with open("model.pkl","wb") as f:
+    pickle.dump(model,f)
