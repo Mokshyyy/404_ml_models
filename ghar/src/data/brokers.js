fb402b04fbee76a750d8d3aa02e4f04712922377
adityapathak9450 <adityapathak9450@gmail.com>
2025-02-08 07:33:36 +0530

    Add files via upload

---
+++ ghar/src/data/brokers.js
@@ -0,0 +1,32 @@
+export const brokers = [
+  {
+    name: "Rajesh Kumar",
+    contact: "+91 9876543210",
+    image: "https://source.unsplash.com/400x300/?man,profile",
+  },
+  {
+    name: "Priya Sharma",
+    contact: "+91 8765432109",
+    image: "https://source.unsplash.com/400x300/?woman,profile",
+  },
+    {
+    name: "Rajesh Kumar",
+    contact: "+91 9876543210",
+    image: "https://source.unsplash.com/400x300/?man,profile",
+  },
+  {
+    name: "Priya Sharma",
+    contact: "+91 8765432109",
+    image: "https://source.unsplash.com/400x300/?woman,profile",
+  },
+     {
+    name: "Rajesh Kumar",
+    contact: "+91 9876543210",
+    image: "https://source.unsplash.com/400x300/?man,profile",
+  },
+  {
+    name: "Priya Sharma",
+    contact: "+91 8765432109",
+    image: "https://source.unsplash.com/400x300/?woman,profile",
+  },
+];
---
+++ ghar/src/data/featuredProperties.js
@@ -0,0 +1,47 @@
+export const featuredProperties = [
+  {
+    title: "Elegant 2BHK Apartment",
+    location: "Varanasi, UP",
+    price: "₹7,500",
+    image:"./images/house1.jpg",
+  },
+  {
+    title: "Modern Duplex Villa",
+    location: "Lucknow, UP",
+    price: "₹9,000",
+    image: "https://source.unsplash.com/400x300/?villa,luxury",
+  },
+
+    
+    {
+    title: "Elegant 2BHK Apartment",
+    location: "Varanasi, UP",
+    price: "₹7,500",
+    image: "https://source.unsplash.com/400x300/?apartment,house",
+  }
+  ,
+  {
+    title: "Modern Duplex Villa",
+    location: "Lucknow, UP",
+    price: "₹9,000",
+    image: "https://source.unsplash.com/400x300/?villa,luxury",
+  },
+  {
+    title: "Spacious Penthouse",
+    location: "Pune, MH",
+    price: "₹8,500",
+    image: "https://source.unsplash.com/400x300/?penthouse,skyline",
+  },
+    {
+    title: "Modern Duplex Villa",
+    location: "Lucknow, UP",
+    price: "₹9,000",
+    image: "https://source.unsplash.com/400x300/?villa,luxury",
+  },
+  {
+    title: "Spacious Penthouse",
+    location: "Pune, MH",
+    price: "₹8,500",
+    image: "https://source.unsplash.com/400x300/?penthouse,skyline",
+  },
+];
