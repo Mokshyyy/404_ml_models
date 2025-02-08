3ad43d8fff4482587ffa162f603d6bab4e615069
adityapathak9450 <adityapathak9450@gmail.com>
2025-02-08 07:41:48 +0530

    Add files via upload

---
+++ ghar/deployment/BangModel.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib
+import numpy as np
+import re
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pickle
+# making dataframes
+raw_df = pd.read_csv('bengaluru_house_prices.csv')
+total_df = raw_df.dropna().copy(0)
+train_df, val_df = train_test_split(total_df, test_size=0.1)
+
+ #  Adjusting size coloumn for numeric values
+total_df.loc[:, 'bhk'] = total_df["size"].apply(lambda x: int(x.split(" ")[0]))
+
+# adjusting area coloumn
+def convert_sqft_to_number(x):
+    tokens = x.split("-")
+    if len(tokens) == 2:
+        return (float(tokens[0])+float(tokens[1]))/2
+    try:
+        return float(x)
+    except:
+        return None
+
+# Remove rows where 'total_sqft' has non-numeric values (units included)
+total_df = total_df[total_df["total_sqft"].str.isnumeric()]
+
+# grouping location with less frequency
+loc_fre = total_df['location'].value_counts()
+total_df['loca'] = total_df['location'].apply(lambda x : x if loc_fre[x] >= 10 else 'other')
+
+# grouping/encoding by mean of location prices
+loc_mean = total_df.groupby('loca')['price'].median()
+total_df['region_encoded'] = total_df['loca'].map(loc_mean)
+
+# Convert the column to numeric
+total_df["total_sqft"] = pd.to_numeric(total_df["total_sqft"])
+total_df = total_df[(total_df["total_sqft"] <= 30000)]
+
+# Define the function to encode the region
+def region_encode(region_name, loc_mean):
+    if region_name in loc_mean:
+        return loc_mean[region_name]
+    else:
+        return None
+print(total_df.price.corr(total_df.bhk))
+#Model building
+model = LinearRegression()
+inputs = total_df[['total_sqft','bhk','bath']]
+target = total_df['price']
+model.fit(inputs,target)
+
+# Prediction
+def predict_price(model, area, bhk,bath,balcony, region_name, loc_mean):
+    region_encoded_value = region_encode(region_name, loc_mean)
+
+    if region_encoded_value is None:
+        return f"Region '{region_name}' not found in the encoding mapping."
+
+    input_data = [[area, bhk,bath,balcony, region_encoded_value]]
+
+    predicted_price = model.predict(input_data)[0]
+
+    return f"Predicted Price for {area} sqft, {bhk} BHK in region '{region_name}': {predicted_price:.2f}"
+prediction = model.predict([[1200,1,1]])
+print(prediction)
+with open("bang_model.pkl","wb") as f:
+    pickle.dump(model,f)
+
+
+
+
+
+
---
+++ ghar/deployment/MumModel.py
@@ -0,0 +1,66 @@
+# Importing libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib
+import pickle
+# Adjusting the plots for better visual
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['figure.figsize'] = (10, 6)
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
+
+# Reading the dataset
+total_df = pd.read_csv("Mumbai House Prices.csv")
+raw_df = total_df[total_df.type == 'Apartment']
+
+# Convert price to numeric scale
+Price_Numeric = {'Cr' : 10000000 , 'L' : 100000}
+Price_Order = raw_df.price_unit.map(Price_Numeric)
+raw_df['Price_Mag'] = Price_Order * raw_df.price # price magnitude
+
+# Split the dataset for train, validation, and testing
+train_val_df, test_df = train_test_split(raw_df, test_size=0.1, random_state=42)
+train_df, val_df = train_test_split(train_val_df, test_size=0.25, random_state=42)
+
+# Target Encoding for Region Column
+region_target_mean = raw_df.groupby('region')['Price_Mag'].median()
+
+# Apply Target Encoding to the 'region' column
+raw_df['region_encoded'] = raw_df['region'].map(region_target_mean)
+
+# Define the function to encode the region
+def region_encode(region_name, region_target_mean):
+    if region_name in region_target_mean:
+        return region_target_mean[region_name]
+    else:
+        return None
+
+# Model building and evaluation
+model = LinearRegression()
+# Apply encoding
+train_df['region_encoded'] = train_df['region'].map(region_target_mean)
+# Fitting inputs and target
+inputs = train_df[['area', 'bhk']]
+targets = train_df['Price_Mag']
+model.fit(inputs, targets)
+
+# Prediction
+def predict_price(model, area, bhk, region_name, region_target_mean):
+    region_encoded_value = region_encode(region_name, region_target_mean)
+
+    if region_encoded_value is None:
+        return f"Region '{region_name}' not found in the encoding mapping."
+
+    input_data = [[area, bhk, region_encoded_value]]
+
+    predicted_price = model.predict(input_data)[0]
+
+    return f"Predicted Price for {area} sqft, {bhk} BHK in region '{region_name}': {predicted_price:.2f}"
+
+# Example Prediction
+prediction = model.predict([[1200,2]])
+print(prediction)
+
+with open("mum_model.pkl","wb") as f:
+    pickle.dump(model,f)
