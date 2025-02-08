7dda37787fa3811ab33abcf49164b8dcce868a94
saurabhmishra13582 <saurabhmishra13582@gmail.com>
2025-02-08 08:48:51 +0530

    Update featuredProperties.js

--- ghar/src/data/featuredProperties.js
+++ ghar/src/data/featuredProperties.js
@@ -3,13 +3,13 @@ export const featuredProperties = [
     title: "Elegant 2BHK Apartment",
     location: "Varanasi, UP",
     price: "₹7,500",
-    image:"./images/house1.jpg",
+    image:"https://th.bing.com/th/id/OIP.m4TH2IUBpjn_ojB9HB0f7wHaFk?w=226&h=180&c=7&r=0&o=5&dpr=1.4&pid=1.7",
   },
   {
     title: "Modern Duplex Villa",
     location: "Lucknow, UP",
     price: "₹9,000",
-    image: "https://source.unsplash.com/400x300/?villa,luxury",
+    image: "https://th.bing.com/th/id/OIP.KF3eIb46-g9pH7D73aa3cgHaFj?w=231&h=180&c=7&r=0&o=5&dpr=1.4&pid=1.7",
   },
 
     
@@ -17,31 +17,31 @@ export const featuredProperties = [
     title: " 3BHK Apartment",
     location: "Varanasi, UP",
     price: "₹9,500",
-    image: "https://source.unsplash.com/400x300/?apartment,house",
+    image: "https://th.bing.com/th/id/OIP.RwzBcCJGCgIoNNo8rTeziQHaDt?w=307&h=174&c=7&r=0&o=5&dpr=1.4&pid=1.7",
   }
   ,
   {
     title: "Verma Resedential",
     location: "Lucknow, UP",
     price: "₹8,000",
-    image: "https://source.unsplash.com/400x300/?villa,luxury",
+    image: "https://th.bing.com/th/id/OIP.H2CUtIcl8O0W59BUfSswzwHaHR?w=169&h=180&c=7&r=0&o=5&dpr=1.4&pid=1.7",
   },
   {
     title: "Spacious Penthouse",
     location: "Pune, MH",
     price: "₹8,500",
-    image: "https://source.unsplash.com/400x300/?penthouse,skyline",
+    image: "https://th.bing.com/th/id/OIP.sCUx2YwOcs0I_IMbl4GRfgHaE8?w=278&h=185&c=7&r=0&o=5&dpr=1.4&pid=1.7",
   },
     {
     title: "Santi Nivaas",
     location: "Lucknow, UP",
     price: "₹9,500",
-    image: "https://source.unsplash.com/400x300/?villa,luxury",
+    image: "https://th.bing.com/th/id/OIP.pKP0Q9jI-0NTCUIpaGpMSAHaE8?w=269&h=180&c=7&r=0&o=5&dpr=1.4&pid=1.7",
   },
   {
     title: "Pathak Nivaas",
     location: "Pune, MH",
     price: "₹6,500",
-    image: "https://source.unsplash.com/400x300/?penthouse,skyline",
+    image: "https://th.bing.com/th/id/OIP.ou-97Be92VfvVQnuZbAjAAHaEo?w=311&h=194&c=7&r=0&o=5&dpr=1.4&pid=1.7",
   },
 ];
